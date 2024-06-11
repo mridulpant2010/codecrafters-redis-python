1d942bb7f1d8e8e3c15d4b2930217dd8a9c2982c
Mridul Pant <mridulpant2010@gmail.com>
2024-06-11 22:25:12 +0530

    added code to handle multiple clients sending multiple requests

--- app/main.py
+++ app/main.py
@@ -1,32 +1,42 @@
-# Uncomment this to pass the first stage
 import socket
+import threading
 
+def handle_connection(conn):
+    while True:
+        data = conn.recv(1024).decode()
+        response = b"+PONG\r\n"
+        if response == b"PONG":
+            break
+        if "PING" in data:
+            conn.sendall(response)
+    conn.close()
 
-def main(HOST,PORT):
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+def main():
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    #server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    #
     with socket.create_server((HOST,PORT),reuse_port=True) as server_socket:
-        conn, address = server_socket.accept()
         while True:
-            data = conn.recv(1024).decode()
-            print("data is ", data)
-            response = b"+PONG\r\n"
-            #try:
-                # result = data.split("\n")
-                # for each_re in result:
-            if "PING" in data:
-                conn.sendall(response)
-            # except Exception as e:
-            #     response = None
-        conn.close()
+            conn, _ =server_socket.accept()
+            thread= threading.Thread(target=handle_connection , args=(conn,))
+            thread.start()
 
 
 if __name__ == "__main__":
     HOST = "127.0.0.1"
     PORT = 6379
-    main(HOST,PORT)
+    main()
+
+
+#one conn and we have multiple request from a single client.
+
+# with socket.create_server((HOST,PORT),reuse_port=True) as server:
+#     conn,add = server.accept()
+#     while True:
+#         data = conn.recv(1024).decode()
+#         print("data is "+data)
+#         if data == b"":
+#             break
+#         response = b"+PONG\r\n"
+#         conn.sendall(response)
+#     conn.close()
+    
+#multiple connection and multiple requests from multiple clients.
