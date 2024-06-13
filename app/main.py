b2b4d4e227e93b33d8f1f15acb06642a6b91a5c2
Mridul Pant <mridulpant2010@gmail.com>
2024-06-13 22:26:56 +0530

    added code for implementing the get and set commands

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import socket
 import threading
+import argparse
 
-#what is asyncore library and how is it useful?
+# what is asyncore library and how is it useful?
 
-class RedisError(Exception):    
+
+class RedisError(Exception):
     pass
 
+
 class Redis:
-    
+
     def parse_response(self, input_string):
         """_summary_
             + : denotes the simple string
@@ -27,38 +30,47 @@ class Redis:
             return intermediate_data
         elif prefix == ":":
             # handle integers
-            print("intermediate is:",intermediate_data)
+            print("intermediate is:", intermediate_data)
             return int(intermediate_data)
         elif prefix == "-":
-            raise RedisError(f"Redis Error {intermediate_data}") # it should return something else as well, right?
+            raise RedisError(
+                f"Redis Error {intermediate_data}"
+            )  # it should return something else as well, right?
         elif prefix == "$":
-            length,data = intermediate_data.split(CRLF,1)
-            return data[:int(length)]
+            length, data = intermediate_data.split(CRLF, 1)
+            return data[: int(length)]
         elif prefix == "*":
-            length, rest_data = intermediate_data.split(CRLF,1)
-            elements=[]
+            length, rest_data = intermediate_data.split(CRLF, 1)
+            elements = []
             for _ in range(int(length)):
-                print("rest_data is: ",rest_data)
-                el ,rest_data = self.parse_response(rest_data)
-                elements.append(el)    
+                print("rest_data is: ", rest_data)
+                el, rest_data = self.parse_response(rest_data)
+                elements.append(el)
             return elements
-                                    
-# line = "$12\r\n*3\r\n:1\r\n:2\r\n:3\r\n\r\n"
-# line.split("\r\n",1)
+
+
 def handle_connection(conn):
     while True:
         data = conn.recv(1024).decode()
-        response = b"+PONG\r\n"
+
         if data == b"":
             break
-        if "PING" in data:
-            conn.sendall(response)
-        
-        if "echo" in data.lower():
-            response_data=data.split(CRLF)[-2]
-            response = f"+{response_data}{CRLF}".encode()
-            conn.sendall(response)
-            
+        response_data = data.split(CRLF)
+        try:
+            if "echo" in data.lower():
+                response_data = response_data[-2]
+                response = f"+{response_data}{CRLF}".encode()
+            elif "set" in data.lower():
+                hash[response_data[4]] = response_data[6]
+                response = f"+OK{CRLF}".encode()
+            elif "get" in data.lower():
+                ans = hash[response_data[4]]
+                response = f"${len(ans)}{CRLF}{ans}{CRLF}".encode()
+            else:
+                response = f"+PONG{CRLF}".encode()
+        except KeyError:
+            response = f"$-1{CRLF}".encode()
+        conn.sendall(response)
     conn.close()
 
 
@@ -75,9 +87,7 @@ if __name__ == "__main__":
     HOST = "127.0.0.1"
     PORT = 6379
     CRLF = "\r\n"
+
+    hash = {}
+
     main()
-    # parser = Redis()
-    # response = "*3\r\n:1\r\n:2\r\n:3\r\n"
-    # #response = "*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n"
-    # parsed_data = parser.parse_response(response)
-    # print(parsed_data)
